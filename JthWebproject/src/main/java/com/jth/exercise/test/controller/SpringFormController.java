473a4ebb37b4d9be5c3eea47324626a0dd37d5fd
21thkafka <82437848+21thkafka@users.noreply.github.com>
2021-10-23 11:37:05 +0900

    MyBatis세팅

---
+++ JthWebproject/src/main/java/com/jth/exercise/test/controller/SpringFormController.java
@@ -0,0 +1,48 @@
+package com.jth.exercise.test.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.jth.exercise.test.vo.OrderRequest;
+
+@Controller
+public class SpringFormController {
+
+	private static final Logger logger = LoggerFactory.getLogger(SpringFormController.class);
+	/**
+	 * 스프링폼 등록화면
+	 * @param model
+	 * @return
+	 * @throws Exception
+	 */
+	@GetMapping("/springform/registerForm")
+	public String registerForm(Model model) throws Exception {
+		
+		logger.info("SpringFormController, registerForm.");
+		
+		model.addAttribute("orderRequest", new OrderRequest());
+		
+		return "springform/registerForm";
+	}
+	/**
+	 * 스프링폼 데이터 전달
+	 * @param orderRequest
+	 * @return
+	 * @throws Exception
+	 */
+	@PostMapping("/springform/register")
+	public String springFormRegisterPost(OrderRequest orderRequest) throws Exception {
+		
+		logger.info("SpringFormController, springFormRegisterPost.");		
+		logger.info("데이터 확인 : {}", orderRequest.getOrderRequestNo());
+		logger.info("데이터 확인 : {}", orderRequest.getOrderRequestDate());
+		logger.info("데이터 확인 : {}", orderRequest.getItemNm());
+		
+		return "springform/result";
+	}
+	
+}
